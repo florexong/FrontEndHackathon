0dda2a209d04489bd60bc9b7b6c6a8bf2aa3d146
Afham Awal-ludin <muhdafham8@gmail.com>
2021-08-25 15:22:01 +0800

    add register function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-fetch('https')
+const form = document.getElementById('registerForm');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let icnumber = document.getElementById('icnumber').value;
+    let password = document.getElementById('password').value;
+
+    fetch('https://mstw-hackathon-api.herokuapp.com/user', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            icnumber: icnumber,
+            password: password
+        })
+    })
+    .then((res) => res.json())
+    .then((data) =>
+        console.log(data))
+});
